cdec3fe7abbf09a2b6a3e42284282e076a6a7f46
septiD <septid96@yahoo.com>
2017-10-22 18:44:26 +0300

    Added in car database and changed the view for Cars

--- RentACar/Controllers/CarController.cs
+++ RentACar/Controllers/CarController.cs
@@ -3,20 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using RentACar.Models;
 
 namespace RentACar.Controllers
 {
     public class CarController : Controller
     {
+        CarsEntities db = new CarsEntities();
         // GET: Car
-        public ActionResult Index()
-        {
-            return View();
-        }
+        
 
         public ActionResult Cars()
         {
-            return View();
+            var cars = from c in db.Tables
+                where c.Available == true
+                select c;
+            return View(cars.ToList());
         }
 
         public ActionResult NewRent()
