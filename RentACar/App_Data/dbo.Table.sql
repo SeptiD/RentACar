aef67d296abb924ae2b1e350582f15076e012de9
septiD <septid96@yahoo.com>
2017-10-24 22:24:04 +0300

    database change

---
+++ RentACar/App_Data/dbo.Table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Table] (
+    [Id]           INT           IDENTITY (1, 1) NOT NULL,
+    [LicencePlate] NVARCHAR (7)  NOT NULL,
+    [Brand]        NVARCHAR (15) NOT NULL,
+    [Name]         NVARCHAR (20) NOT NULL,
+    [Available]    BIT           NOT NULL,
+    [Description]  TEXT          NULL,
+    [Image]        VARBINARY(MAX)         NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
